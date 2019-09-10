f15f593d662feaa3ae4568e87eca8f5fa7d73b37
Guto <luiz.v.nasc@gmail.com>
2019-09-10 10:03:40 -0300

    Criado server para o heroku

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
-// cwbus.io é uma aplicação que monitora os serviços de dados abertos da URBS sobre o transporte 
+// cwbus.io é uma aplicação que monitora os serviços de dados abertos da URBS sobre o transporte
 // público de Curitiba e armazena um histórico em uma base pública.
 package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/luizvnasc/cwbus.io/store"
 )
 
 func main() {
@@ -20,6 +23,26 @@ func main() {
 	}
 
 	store := store.NewMongoStore(ctx, client)
-	//TODO: CONSUMERS VEM AQUI
+
+	http.HandleFunc("/versao",Versao)
+	log.Fatalf("%q\n", http.ListenAndServe(Addr(),nil))
+
 	store.Disconnect()
+}
+
+// Versao é handler de chamada http para o caminho /versao.
+func Versao(w http.ResponseWriter, r *http.Request ){
+	fmt.Fprintf(w,"0.0.1")
+}
+
+// Addr cria o endereço onde a aplicação estará rodando.
+func Addr() string {
+	if len(os.Getenv("$PORT")) == 0 {
+		return ":8081"
+	}
+	return ":" + os.Getenv("$PORT")
+}
+
+func wakeUp(url string, sleepTime int64){
+
 }
