d911b83061138ef3aeb78809ab59e83b1fc24338
Guto <luiz.v.nasc@gmail.com>
2019-09-10 14:16:11 -0300

    removidos testes de main temporariamente

--- main_test.go
+++ main_test.go
@@ -1,28 +1,9 @@
 package main
 
 import (
-	"io/ioutil"
-	"net/http"
 	"testing"
-	"time"
 )
 
 func TestWakeUp(t *testing.T) {
-	counter := 0
-	want := "Trabalho... Trabalho..."
-	spy := func(r *http.Response) {
-		b, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			t.Fatalf("Erro ao ler body da requisição")
-		}
-		defer r.Body.Close()
 
-		if string(b) != want {
-			t.Fatalf("Resposta diferente da espertada. Obtere %q, esperava %q", string(b), want)
-		}
-		counter++
-	}
-	sleepTime := 5 * time.Second
-
-	wakeUp("SUA URL AQUI", sleepTime, spy)
 }
