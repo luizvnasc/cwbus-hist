248b3dbfa622ff0c47a3dd133f06f10e6ea847cb
Guto <luiz.v.nasc@gmail.com>
2019-09-13 09:09:46 -0300

    Limpeza de pacotes nao utilizados

--- jobs/jobs.go
+++
@@ -1,2 +0,0 @@
-// Package jobs contém os jobs que serão executados pela aplicação para consumir os dados da urbs
-package jobs
--- jobs/jobs_test.go
+++
@@ -1,9 +0,0 @@
-package jobs
-
-import (
-	"testing"
-)
-
-func TestWakeUp(t *testing.T) {
-
-}
