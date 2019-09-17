9cbdd6c30e22ca1bc66fb8b4ec230d508031e733
Luiz Augusto Volpi Nascimento <luiz.v.nasc@gmail.com>
2019-09-16 21:35:53 -0300

    Adicionado teste caminho feliz wakeupdyno

--- scheduler/app_test.go
+++ scheduler/app_test.go
@@ -2,7 +2,6 @@ package scheduler
 
 import (
 	"bytes"
-	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -26,7 +25,6 @@ func TestAppScheduler(t *testing.T) {
 		s := NewAppScheduler()
 		s.wakeUpDyno()
 		got := buf.String()
-		fmt.Printf("got: %s", got)
 		if !strings.Contains(got, "Erro ao acordar o dyno:") {
 			t.Errorf("Erro ao validar url na task wakeup.")
 		}
@@ -45,8 +43,25 @@ func TestAppScheduler(t *testing.T) {
 		s := NewAppScheduler()
 		s.wakeUpDyno()
 		got := buf.String()
-		fmt.Printf("got: %s", got)
 		if !strings.Contains(got, "Erro ao acordar o dyno, Status:") {
+			t.Errorf("Erro ao validar status na task wakeup.")
+		}
+	})
+
+	t.Run("Teste acordar dyno", func(t *testing.T) {
+		var buf bytes.Buffer
+
+		log.SetOutput(&buf)
+		defer func() {
+			log.SetOutput(os.Stderr)
+		}()
+
+		os.Setenv("CWBUS_WAKEUP_URL", "https://httpstat.us/200")
+
+		s := NewAppScheduler()
+		s.wakeUpDyno()
+		got := buf.String()
+		if !strings.Contains(got, "Trabalho...") {
 			t.Errorf("Erro ao validar status na task wakeup.")
 		}
 	})
